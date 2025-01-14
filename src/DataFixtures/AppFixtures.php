f990c0bcf7e5492283e66a23bb4c10aba71d42ed
Tiago Machado <tiago.machado.1987@gmail.com>
2025-01-14 21:11:09 +0100

    Add data fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Entity\Products;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $productsFile = __DIR__ . '/data/products.json';
+        $usersFile = __DIR__ . '/data/users.json';
+        $dataProducts = json_decode(file_get_contents($productsFile), true);
+        $dataUsers = json_decode(file_get_contents($usersFile), true);
+
+        foreach ($dataProducts as $item) {
+            $product = new Products();
+            $product->setName($item['name']);
+            $product->setStock($item['stock']);
+            $product->setPrice($item['price']);
+            $product->setHighLighted($item['high_lighted']);
+            $product->setImage($item['image']);
+
+            $manager->persist($product);
+        }
+
+        foreach ($dataUsers as $item) {
+            $user = new User();
+            $user->setEmail($item['email']);
+            $user->setPassword($this->passwordHasher->hashPassword($user, $item['password']));
+            $user->setRoles($item['roles']);
+            $user->setName($item['name']);
+            $user->setIsVerified($item['is_verified']);
+            $user->setDeliveryAddress($item['delivery_adress']);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
